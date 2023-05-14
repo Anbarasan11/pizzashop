9eb7e4ef8cc028c3d0c0ceb0f3146f9bf6557e78
Anbarasan <118906298+arasan420@users.noreply.github.com>
2023-05-14 16:58:57 +0530

    pizzashop

---
+++ src/components/Comments.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container , Row , Col ,Button , Form } from 'react-bootstrap';
+
+
+const Comments = () => {
+  return (
+    <section id='contact' className='bg-light'>
+         <div className="text-center my-3  py-3" >
+            <h2 className=' text-primary'> Comments  <i class="bi bi-chat-left-heart"></i></h2>
+        </div>
+        <Container>
+            <Row>
+                <Col md={6}>
+                    <Form>
+                        
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+
+    </section>
+  )
+}
+
+export default Comments
